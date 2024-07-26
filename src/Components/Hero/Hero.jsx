2cacc52d89758cb1445f33763f30b1bd16484faf
AlishaAroob <aroob.dev@gmail.com>
2024-07-26 22:22:53 +0500

    added text to hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,8 +4,12 @@ import "./Hero.css"
 
 const Hero = () => {
     return (
-        <div className="hero">
-
+        <div className="hero container">
+            <div className="hero-text">
+                <h1>we ensure better education for better world</h1>
+                <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education </p>
+                <div className="btn">Explore more</div>
+            </div>
         </div>
     )
 }

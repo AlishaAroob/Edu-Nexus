d7098790c2ae3fbb3216deff687c9f99c8034b05
AlishaAroob <aroob.dev@gmail.com>
2024-07-28 00:15:53 +0500

    added images to program

--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import "../../Components/Programs/Programs.css"
+import "./Programs.css"
+import Program1 from "../../assets/program-1.png"
+import Program2 from "../../assets/program-2.png"
+import Program3 from "../../assets/program-3.png"
+
 const Programs = () => {
     return (
-        <div>
+        <div className='Programs'>
+            <div className="Program">
+                <img src={Program1} alt='' />
+                <img src={Program2} alt='' />
+                <img src={Program3} alt='' />
 
+            </div>
         </div>
     )
 }

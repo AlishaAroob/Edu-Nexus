f37147979638f066b6bebabcac3ad56562ade43f
AlishaAroob <aroob.dev@gmail.com>
2024-07-28 20:05:18 +0500

    created scroll for navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,20 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./Navbar.css"
 import Logo from "../../assets/Logo.png"
 
 const Navbar = () => {
+
+    const [sticky, setsticky] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            window.scrollY > 50 ? setsticky(true) : setsticky(false)
+        })
+    }, []
+    )
+
     return (
-        <nav className="container">
+        <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
             <img src={Logo} alt="" className='logo' />
             <h1>EDU-NEXUS</h1>
             <ul>

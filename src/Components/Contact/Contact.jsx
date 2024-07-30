9112a44a1c2b0979023548e1e578f514cc3aa55e
AlishaAroob <aroob.dev@gmail.com>
2024-07-31 01:09:48 +0500

    completed form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,6 +7,32 @@ import Location from "../../assets/location-icon.png"
 import White_icon from "../../assets/white-arrow.png"
 
 const Contact = () => {
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "c167a8bf-fbb0-403a-a640-08ac45c295c9");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+
+        } else {
+            console.log("Error", data);
+            setResult(data.message);
+        }
+    };
+
     return (
         <div className='contact'>
             <div className="contact-col">
@@ -26,15 +52,16 @@ const Contact = () => {
                 </ul>
             </div>
             <div className="contact-col">
-                <form>
+                <form onSubmit={onSubmit}>
                     <label>Name</label>
                     <input type='text' name='name' placeholder='Enter your name ' required />
                     <label>Phone Number</label>
                     <input type='tel' name='phone' placeholder='Enter your phone number ' required />
                     <label>Message</label>
-                    <textarea type='text' name='message' placeholder='Enter your message here ' rows="6" required />
+                    <textarea name='message' placeholder='Enter your message here ' rows="6" required />
                     <button className='btn1 dark-btn'>Submit Now <img src={White_icon} alt='' /> </button>
                 </form>
+                <span>{result}</span>
             </div>
 
         </div>
